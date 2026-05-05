97d330190eb57cca94e9ae65e9ee6163dcda9b5b
YaLTeR <YaLTeR@users.noreply.github.com>
2026-05-05 19:37:24 +0000

    deploy: 56654034e9a9b74f6fcf268ebfe114a8c74a8c0b

---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["niri_ipc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.95.0/core/time/struct.Duration.html\" title=\"struct core::time::Duration\">Duration</a>&gt; for <a class=\"struct\" href=\"niri_ipc/struct.Timestamp.html\" title=\"struct niri_ipc::Timestamp\">Timestamp</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"niri_ipc/struct.Timestamp.html\" title=\"struct niri_ipc::Timestamp\">Timestamp</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.95.0/core/time/struct.Duration.html\" title=\"struct core::time::Duration\">Duration</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[838]}
